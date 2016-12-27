499e3d8661f814ca4350994c91dbd493cb89efee
Zeno Zeng <zenoofzeng@gmail.com>
2016-12-27 22:44:46 +0800

    readFile

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 
 	"log"
 
+	"io/ioutil"
+
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -30,15 +32,26 @@ func main() {
 			Name:  "build",
 			Usage: "fossil build [FILE]...",
 			Action: func(c *cli.Context) error {
-				files := make(map[string]string, 0)
+				files := make(map[string][]byte, 0)
 				for _, pattern := range c.Args() {
 					matches, err := filepath.Glob(pattern)
 					if err != nil {
 						return err
 					}
 					for _, file := range matches {
-						log.Printf("Found: %v", file)
-						files[file] = ""
+						fileInfo, err := os.Stat(file)
+						if err != nil {
+							return err
+						}
+						fileMode := fileInfo.Mode()
+						if fileMode.IsDir() {
+							continue
+						}
+						files[file], err = ioutil.ReadFile(file)
+						if err != nil {
+							return err
+						}
+						log.Printf("File: %v, Size: %v bytes", file, fileInfo.Size())
 					}
 				}
 				return nil
