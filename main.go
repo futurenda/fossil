d9d35465e2e473f6037b57e8c74f0cb60051bf74
Zeno Zeng <zenoofzeng@gmail.com>
2016-12-27 22:37:42 +0800

    glob

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"os"
+	"path/filepath"
+
+	"flag"
+
+	"log"
 
 	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
+	flag.Parse()
 	app := cli.NewApp()
 	app.Name = "fossil"
 	app.Version = "0.0.1"
@@ -18,5 +24,27 @@ func main() {
 			Usage: "",
 		},
 	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "build",
+			Usage: "fossil build [FILE]...",
+			Action: func(c *cli.Context) error {
+				files := make(map[string]string, 0)
+				for _, pattern := range c.Args() {
+					matches, err := filepath.Glob(pattern)
+					if err != nil {
+						return err
+					}
+					for _, file := range matches {
+						log.Printf("Found: %v", file)
+						files[file] = ""
+					}
+				}
+				return nil
+			},
+		},
+	}
+
 	app.Run(os.Args)
 }
